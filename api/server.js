33f650b3e24fa182b7e60f8a4ad4d88c00b696d2
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-02-12 23:47:20 -0500

    added helmet and morgan middleware required knex in server added users and notes routes

--- api/server.js
+++ api/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const helmet = require('helmet');
+const logger = require('morgan');
+const knex = require('knex');
+
+const usersRoute = require('./routes/users');
+const notesRoute = require('./routes/notes');
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(logger('dev'));
+
+// routes
+server.use('/api/users', usersRoute);
+server.use('/api/notes', notesRoute);
 
 server.get('/', async (req, res) => {;
   res.status(200).json({ api: 'up and running' })
