a7dbe9e8bb47414caf80aed7d6652c13fe3574b5
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-02-13 22:01:10 -0500

    corrected route pathways

--- api/server.js
+++ api/server.js
@@ -3,8 +3,8 @@ const helmet = require('helmet');
 const logger = require('morgan');
 const knex = require('knex');
 
-const usersRoute = require('./routes/users');
-const notesRoute = require('./routes/notes');
+const usersRoute = require('../routes/users');
+const notesRoute = require('../routes/notes');
 
 const server = express();
 
@@ -16,7 +16,7 @@ server.use(logger('dev'));
 server.use('/api/users', usersRoute);
 server.use('/api/notes', notesRoute);
 
-server.get('/', async (req, res) => {;
+server.get('/', async (req, res) => {
   res.status(200).json({ api: 'up and running' })
 });
 
